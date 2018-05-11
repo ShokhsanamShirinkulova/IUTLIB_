1c4e8044d78cf7e5f05fc162cef042f5e9fbb87d
SardorAllaberganov <sardorallaberganov96@gmail.com>
2018-05-11 17:20:09 +0500

    catalog change 05/11/2018

--- resources/views/pages/catalog.blade.php
+++ resources/views/pages/catalog.blade.php
@@ -80,7 +80,25 @@
           
                       <div class="col-md-4">
                         <div class="book-info">
-                          <h5 style="margin-bottom: 15px;">Overall Rating: {{($book->bookRank/5)*100}}% <span class="glyphicon glyphicon-star"></span></h5>
+                          <h5 style="margin-bottom: 15px;">
+                            <div class="ratingBlock">
+                              <span>Rating:</span>
+                              <div class="stars">
+                                <form action="">
+                                  <input class="star star-5" id="star-5" type="radio" name="star"/>
+                                  <label class="star star-5" for="star-5"></label>
+                                  <input class="star star-4" id="star-4" type="radio" name="star"/>
+                                  <label class="star star-4" for="star-4"></label>
+                                  <input class="star star-3" id="star-3" type="radio" name="star"/>
+                                  <label class="star star-3" for="star-3"></label>
+                                  <input class="star star-2" id="star-2" type="radio" name="star"/>
+                                  <label class="star star-2" for="star-2"></label>
+                                  <input class="star star-1" id="star-1" type="radio" name="star"/>
+                                  <label class="star star-1" for="star-1"></label>
+                                </form>
+                              </div>
+                            </div>
+                          </h5>
                           <h5>Genre: <a href="#">{{$book->genre}}</a></h5>
                           <h5>Country: {{$book->country}}</h5>
                           <h5>Language: {{$book->language}}</h5>
